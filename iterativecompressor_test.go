b10057bdabd45e1bd3dc087845390a5ca6f5136d
Zack Scholl <zack.scholl@gmail.com>
2017-12-22 10:06:25 -0700

    Add benchmark. Compressing is about 1.7x slower

--- iterativecompressor_test.go
+++ iterativecompressor_test.go
@@ -1,10 +1,32 @@
 package iterativecompressor
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func BenchmarkNormalMarshal(b *testing.B) {
+	a := make(map[string]interface{})
+	a["zack"] = -42
+	a["88:bb:cc"] = "some text"
+	a["bb:dd:ee:ff"] = "some other text"
+	for n := 0; n < b.N; n++ {
+		json.Marshal(a)
+	}
+}
+
+func BenchmarkCompressing(b *testing.B) {
+	a := make(map[string]interface{})
+	a["zack"] = -42
+	a["88:bb:cc"] = "some text"
+	a["bb:dd:ee:ff"] = "some other text"
+	for n := 0; n < b.N; n++ {
+		ic, _ := New()
+		ic.Dumps(a)
+	}
+}
 
 func TestCompression(t *testing.T) {
 	a := make(map[string]interface{})
